d3fd0b501e8f80ea3fe468fb17e9711fb56e333e
Danilo Lemes <daniloleemes@hotmail.com>
2023-04-30 12:54:12 +0100

    fix streffectrenderer editor

--- Editor/StrEffectRendererEditor.cs
+++ Editor/StrEffectRendererEditor.cs
@@ -1,13 +1,14 @@
-﻿using UnityEditor;
+﻿using Core.Effects;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(StrEffectRenderer))]
 internal class StrEffectRendererEditor : Editor {
     public override void OnInspectorGUI() {
-        var component = (StrEffectRenderer) target;
+        var component = (StrEffectRenderer)target;
         base.OnInspectorGUI();
 
-        if(GUILayout.Button("Replay Effect")) {
+        if (GUILayout.Button("Replay Effect")) {
             // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
             component.Replay();
         }
