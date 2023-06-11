67e2b8d4b3ea4668e7743cfb6ca7eab5189571f7
Danilo Menezes <danilo.menezes@sky.uk>
2023-06-11 22:02:20 +0100

    Change camera to use new input system

--- Core/Camera/CharacterCamera.cs
+++ Core/Camera/CharacterCamera.cs
@@ -9,16 +9,13 @@ namespace UnityRO.Core.Camera {
     public class CharacterCamera : MonoBehaviour {
         #region Serializables
 
-        [Header(":: Refs")]
-        public UnityCamera GameCamera;
-        
-        [Header(":: User Parameters")]
-        public Vector2 MouseSensitivity = Vector2.one;
+        [Header(":: Refs")] public UnityCamera GameCamera;
+
+        [Header(":: User Parameters")] public Vector2 MouseSensitivity = Vector2.one;
         public float ScrollPitchSensitivity = 1f;
         public float ScrollZoomSensitivity = 1f;
-        
-        [Header(":: Settings")]
-        public CameraControlProfile YawControl;
+
+        [Header(":: Settings")] public CameraControlProfile YawControl;
         public CameraControlProfile ZoomControl;
 
         public float LerpTime = 0.5f;
@@ -26,8 +23,7 @@ namespace UnityRO.Core.Camera {
         public Vector2 ZoomConstraint;
         public Vector2 PitchConstraint;
 
-        [SerializeField]
-        private Transform m_Target;
+        [SerializeField] private Transform m_Target;
 
         public Direction Direction;
         public Vector3 HorizontalDirection { get; private set; }
@@ -38,7 +34,7 @@ namespace UnityRO.Core.Camera {
         private float m_Yaw = 7.869574f;
         private float m_Altitude = 0.7071068f;
         private float m_SphereSliceRadius = 0.7071068f;
-        
+
         // cache
         private readonly float s_PI2 = Mathf.PI * 2f;
         private Vector2 m_PitchConstraintRad = new(0.5235988f, 0.7853982f);
@@ -55,47 +51,6 @@ namespace UnityRO.Core.Camera {
         }
 
         private void Update() {
-            var dt = Time.deltaTime;
-            var shiftModifier = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-            var mouseScroll = Input.mouseScrollDelta.y;
-
-            if (Input.GetMouseButton(1)) {
-                var hX = Input.GetAxis("Mouse X");
-                var hY = Input.GetAxis("Mouse Y");
-
-                if (shiftModifier) {
-                    var vScroll = hY * MouseSensitivity.y + mouseScroll * ScrollPitchSensitivity;
-                    if (vScroll != 0f) {
-                        Pitch -= vScroll * dt;
-                        RecomputeCameraAngle();
-                    }
-                } else {
-                    YawControl.SetInertia(hX * MouseSensitivity.x);
-                }
-            } else if (Input.GetMouseButtonUp(1)) {
-                if (!shiftModifier) {
-                    YawControl.Release();
-                }
-            }
-
-            if (!shiftModifier && mouseScroll != 0f) {
-                ZoomControl.SetInertia(mouseScroll * ScrollZoomSensitivity);
-                ZoomControl.Release();
-            }
-
-            if (YawControl.Update(dt)) {
-                m_Yaw -= YawControl.Velocity * dt;
-                if (m_Yaw < 0f) {
-                    m_Yaw += s_PI2;
-                }
-                RecomputeHorizontalDirection();
-            }
-            
-            if (ZoomControl.Update(dt)) {
-                var zoomVel = ZoomControl.Velocity * dt;
-                Distance = Mathf.Clamp(Distance + zoomVel, ZoomConstraint.x, ZoomConstraint.y);
-            }
-            
             UpdateCameraPosition();
             UpdateCameraLookAt();
         }
@@ -119,18 +74,19 @@ namespace UnityRO.Core.Camera {
                 //m_Target.transform.rotation = transform.rotation;
             }
 
-            var angle = (float) ((m_Yaw + Math.PI / 8f) / (2f * Math.PI));
+            var angle = (float)((m_Yaw + Math.PI / 8f) / (2f * Math.PI));
 
             if (angle < 0f)
                 angle += 1f;
 
             var orientedAngle = angle - 1f / 4f;
-            var direction = (int) (orientedAngle * 8) % 8;
-            Direction = (Direction) direction;
+            var direction = (int)(orientedAngle * 8) % 8;
+            Direction = (Direction)direction;
         }
 
         private void RecomputeHorizontalDirection() {
-            HorizontalDirection = new Vector3(Mathf.Cos(m_Yaw) * m_SphereSliceRadius, 0f, Mathf.Sin(m_Yaw) * m_SphereSliceRadius);
+            HorizontalDirection = new Vector3(Mathf.Cos(m_Yaw) * m_SphereSliceRadius, 0f,
+                                              Mathf.Sin(m_Yaw) * m_SphereSliceRadius);
         }
 
         private void RecomputeCameraAngle() {
@@ -138,6 +94,46 @@ namespace UnityRO.Core.Camera {
             m_Altitude = Mathf.Sin(Pitch);
             m_SphereSliceRadius = Mathf.Cos(Pitch);
             RecomputeHorizontalDirection();
+        }
+
+        public void OnRotatePressed(Vector2 mouseDelta, float mouseScroll, bool isShiftModified) {
+            if (isShiftModified) {
+                var vScroll = mouseDelta.y * MouseSensitivity.y + mouseScroll * ScrollPitchSensitivity;
+                if (vScroll == 0f) return;
+
+                Pitch -= vScroll * Time.deltaTime;
+                RecomputeCameraAngle();
+            } else {
+                YawControl.SetInertia(mouseDelta.x * MouseSensitivity.x);
+            }
+            
+            var dt = Time.deltaTime;
+            if (!YawControl.Update(dt)) return;
+            
+            m_Yaw -= YawControl.Velocity * dt;
+            if (m_Yaw < 0f) {
+                m_Yaw += s_PI2;
+            }
+            RecomputeHorizontalDirection();
+        }
+
+        public void OnRotateReleased(bool isShiftModified) {
+            if (!isShiftModified) {
+                YawControl.Release();
+            }
+        }
+
+        public void OnZoom(float mouseScroll, bool isShiftModified) {
+            var dt = Time.deltaTime;
+            if (!isShiftModified || mouseScroll != 0f) {
+                ZoomControl.SetInertia(mouseScroll * ScrollZoomSensitivity);
+                ZoomControl.Release();
+            }
+
+            if (ZoomControl.Update(dt)) {
+                var zoomVel = ZoomControl.Velocity * dt;
+                Distance = Mathf.Clamp(Distance + zoomVel, ZoomConstraint.x, ZoomConstraint.y);
+            }
         }
     }
 }
