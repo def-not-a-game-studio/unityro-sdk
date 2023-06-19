65c00c9df743e0cd5a157ec6d509fa62af15bce1
Danilo Menezes <danilo.menezes@sky.uk>
2023-06-19 22:11:50 +0100

    SELECT_ACCESSIBLE_MAPNAME.cs

--- Net/Packets/In/HC/NOTIFY_ACCESSIBLE_MAPNAME.cs
+++ Net/Packets/In/HC/NOTIFY_ACCESSIBLE_MAPNAME.cs
@@ -1,4 +1,6 @@
-﻿using ROIO.Utils;
+﻿using System.Collections.Generic;
+using ROIO.Utils;
+using UnityEngine;
 
 public partial class HC {
 
@@ -6,8 +8,9 @@ public partial class HC {
     public class NOTIFY_ACCESSIBLE_MAPNAME : InPacket {
 
         public const PacketHeader HEADER = PacketHeader.HC_NOTIFY_ACCESSIBLE_MAPNAME;
-
         public PacketHeader Header => HEADER;
+
+        public List<MapInfo> mapinfoList = new();
 
         public void Read(MemoryStreamReader br, int size) {
             var count = (br.Length - br.Position) / 20;
@@ -15,7 +18,17 @@ public partial class HC {
             for(var i = 0; i < count; i++) {
                 var status = br.ReadInt();
                 var mapname = br.ReadBinaryString(16);
+                Debug.Log($"MapEntry {status} {mapname}");
+                mapinfoList.Add(new MapInfo {
+                    status = status,
+                    mapName = mapname
+                });
             }
+        }
+
+        public struct MapInfo {
+            public int status;
+            public string mapName;
         }
     }
 }
---
+++ Net/Packets/Out/CH/SELECT_ACCESSIBLE_MAPNAME.cs
@@ -0,0 +1,21 @@
+﻿public partial class CH {
+
+    public class SELECT_ACCESSIBLE_MAPNAME : OutPacket {
+
+        private const PacketHeader HEADER = PacketHeader.CH_SELECT_ACCESSIBLE_MAPNAME;
+        private const int SIZE = 4;
+
+        public int slot;
+        public int mapNumber;
+
+        public SELECT_ACCESSIBLE_MAPNAME() : base(HEADER, SIZE){
+        }
+
+        public override void Send() {
+            Write((byte)slot);
+            Write((byte)mapNumber);
+
+            base.Send();
+        }
+    }
+}
