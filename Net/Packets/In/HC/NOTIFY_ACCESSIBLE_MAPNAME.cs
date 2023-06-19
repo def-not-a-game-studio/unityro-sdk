fcdd4e744d9d5761a218ccf28a145e53fc246caa
Danilo Menezes <danilo.menezes@sky.uk>
2023-06-19 21:32:15 +0100

    HC_NOTIFY_ACCESSIBLE_MAPNAME

---
+++ Net/Packets/In/HC/NOTIFY_ACCESSIBLE_MAPNAME.cs
@@ -0,0 +1,21 @@
+﻿using ROIO.Utils;
+
+public partial class HC {
+
+    [PacketHandler(HEADER, "HC_NOTIFY_ACCESSIBLE_MAPNAME")]
+    public class NOTIFY_ACCESSIBLE_MAPNAME : InPacket {
+
+        public const PacketHeader HEADER = PacketHeader.HC_NOTIFY_ACCESSIBLE_MAPNAME;
+
+        public PacketHeader Header => HEADER;
+
+        public void Read(MemoryStreamReader br, int size) {
+            var count = (br.Length - br.Position) / 20;
+
+            for(var i = 0; i < count; i++) {
+                var status = br.ReadInt();
+                var mapname = br.ReadBinaryString(16);
+            }
+        }
+    }
+}
