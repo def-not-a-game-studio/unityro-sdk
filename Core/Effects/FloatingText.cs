cfae4b47ced3bec4fca35b98492e3106953695cf
Danilo Lemes <daniloleemes@hotmail.com>
2023-08-12 22:42:20 +0100

    Clean up some animations

--- Core/Effects/FloatingText.cs
+++ Core/Effects/FloatingText.cs
@@ -9,6 +9,41 @@ namespace UnityRO.Core.Effects {
         [SerializeField]
         private TextMeshPro TextField;
 
+        public void SetText(EntityActionRequest actionRequest, Action<FloatingText> onDestroy) {
+            switch (actionRequest.action) {
+                    case ActionRequestType.ATTACK_MULTIPLE_NOMOTION:
+                    case ActionRequestType.ATTACK:
+                        SetText($"{actionRequest.damage}", Color.white, onDestroy);
+                        //target.Damage(pkt.damage, GameManager.Tick + pkt.sourceSpeed);
+                        break;
+
+                    // double attack
+                    case ActionRequestType.ATTACK_MULTIPLE:
+                        SetText($"{actionRequest.damage}", Color.white, onDestroy);
+                        // Display combo only if entity is mob and the attack don't miss
+                        // if (dstEntity.Type == EntityType.MOB && pkt.damage > 0) {
+                        //     dstEntity.Damage(pkt.damage / 2, GameManager.Tick + pkt.sourceSpeed * 1, DamageType.COMBO);
+                        //     dstEntity.Damage(pkt.damage, GameManager.Tick + pkt.sourceSpeed * 2, DamageType.COMBO | DamageType.COMBO_FINAL);
+                        // }
+
+                        // target.Damage(pkt.damage / 2, GameManager.Tick + pkt.sourceSpeed * 1);
+                        // target.Damage(pkt.damage / 2, GameManager.Tick + pkt.sourceSpeed * 2);
+                        break;
+
+                    // TODO: critical damage
+                    case ActionRequestType.ATTACK_CRITICAL:
+                        SetText($"{actionRequest.damage}", Color.white, onDestroy);
+                        // target.Damage(pkt.damage, GameManager.Tick + pkt.sourceSpeed);
+                        break;
+
+                    // TODO: lucky miss
+                    case ActionRequestType.ATTACK_LUCKY:
+                        SetText($"{actionRequest.damage}", Color.white, onDestroy);
+                        // target.Damage(0, GameManager.Tick + pkt.sourceSpeed);
+                        break;
+                }
+        }
+
         public void SetText(string text, Color? color, Action<FloatingText> onDestroy) {
             TextField.text = text;
             TextField.color = color ?? Color.white;
